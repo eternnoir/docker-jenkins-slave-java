40f25897000bad0dddce0cac91cb20f088d0bb82
FrankWang <eternnoir@gmail.com>
2015-01-25 02:16:03 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /home/jenkins http://master:$MASTER_PORT_8080_TCP_PORT/jnlpJars/slave.jar
-java -jar /home/jenkins/slave.jar -jnlpUrl http://master:$MASTER_PORT_8080_TCP_PORT/computer/$SLAVE_ID/slave-agent.jnlp
+wget -P /home/jenkins http://$MASTER_ADDR/jnlpJars/slave.jar
+java -jar /home/jenkins/slave.jar -jnlpUrl http://$MASTER_ADDR/computer/$SLAVE_ID/slave-agent.jnlp
